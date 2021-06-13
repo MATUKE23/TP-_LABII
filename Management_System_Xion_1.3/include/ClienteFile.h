f6915e09d543f0fdd5ce670da3cb36fef7fbbfd5
MATIAS <65511002+MATUKE23@users.noreply.github.com>
2021-06-13 11:44:33 -0300

    Add files via upload

---
+++ Management_System_Xion_1.3/include/ClienteFile.h
@@ -0,0 +1,22 @@
+#define FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#ifndef CLIENTEFILE_H
+#define CLIENTEFILE_H
+
+
+class ClienteFile
+{
+	private:
+		FILE *pfile;
+
+	public:
+		//static ClienteNegocio x;
+		bool grabarEnDisco_Cliente(*pfile );
+		//bool leerDeDisco_Cliente(Cliente_Proveedor_Propiedades &Cliente,int pos);
+
+};
+
+
+
+
+#endif // CLIENTEFILE_H
---
+++ Management_System_Xion_1.3/src/ClienteNegocio.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<clocale>
+#include<cstdlib>
+#include "ClienteFile.h""
+#include "ClienteNegocio.h"
+
+#include "ClienteVista.h"
+
+
+using namespace std;
+
+/*
+ bool grabarEnDisco_Cliente(ClienteNegocio x){
+        FILE *p;
+        p=fopen("Cliente.dat","ab");
+        if(p==NULL) return false;
+        bool escribio=fwrite(&x, sizeof (ClienteNegocio), 1, p);
+        fclose(p);
+        return escribio;
+    }
+
+*/
+
+bool ClienteNegocio::ValidacionCliente ( int IDcliente )
+{
+	if(IDcliente<=0)
+	{
+		cout<<"Ingrese un Id de cliente valido. "<<endl;
+		return false;
+	}
+ClienteNegocio x;
+	if(grabarEnDisco_Cliente(*x))return true;
+}
